8ec2a150635919d5dfefe58b58b03de8344d2a4e
OlehDzhumyk <Oleh.Dzhumyk.IR.2021@lpnu.ua>
2022-05-04 17:12:12 +0200

    Add test for writer.

--- src/test/java/ua/lviv/iot/hospitalManeger/HospitalManagerImplTest.java
+++ src/test/java/ua/lviv/iot/hospitalManeger/HospitalManagerImplTest.java
@@ -1,5 +1,6 @@
 package ua.lviv.iot.hospitalManeger;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ua.lviv.iot.hospitalModel.Department;
@@ -8,6 +9,9 @@ import ua.lviv.iot.hospitalModel.departmentOfPediatrics.Pediatrician;
 import ua.lviv.iot.hospitalModel.departmentOfSurgery.Neurosurgeon;
 import ua.lviv.iot.hospitalModel.paramedic.Paramedic;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -89,6 +93,23 @@ class HospitalManagerImplTest {
 
     }
 
+    @Test
+    void fileEqualTest() throws IOException {
+        try (
+                FileReader expected = new FileReader("src/test/java/testResources/doctorList/expected.csv");
+                BufferedReader expectedBuf = new BufferedReader(expected);
+
+                FileReader actual = new FileReader("src/main/resources/doctorList/result.csv");
+                BufferedReader actualBuf = new BufferedReader(actual)
+
+        ) {
+            String line = "";
+            while (line != null) {
+                Assertions.assertEquals(expectedBuf.readLine(), line = actualBuf.readLine());
+            }
+        }
+
+    }
 
 
 }
